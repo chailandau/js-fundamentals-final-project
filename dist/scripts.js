6f315c9fb8741e2fdd4c72cb215206c657bdc637
Chai Landau <chai.landau@gmail.com>
2020-12-29 22:56:35 -0500

    integrating tenor api

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,5 +1,21 @@
 ( function() 
 {
+  // API call
+  let apiCall = async() => 
+  {
+    try
+    {
+      let data = await axios(
+        "https://api.tenor.com/v1/search?cats"
+      );
+      return data;
+    }
+    catch( err ) 
+    {
+      return console.error( "Error:", err );
+    }
+  };
+  
   // define buttons
   const button1 = document.querySelector( "#button_1" );
   const button4 = document.querySelector( "#button_4" );
@@ -13,37 +29,41 @@
   // define gif results
   const gifResults = document.querySelector( "#gif_results" );
 
-  // form handler
-
-  gifForm.addEventListener ( "submit", event=> 
+  // After API is loaded...
+  apiCall().then( gifs => 
   {
-    event.preventDefault();
-    const searchTitle = document.createElement( "p" );
-    searchTitle.textContent = searchGifs.value;
-    gifResults.appendChild( searchTitle );
-
-    console.log( searchGifs.value );
-    if( button1.checked ) 
-    { 
-      const image = document.createElement( "img" );
-      image.src = "Click Me!";
-      gifResults.appendChild( image );    
-    }
-    else if( button4.checked ) 
-    { 
-      console.log( "4 checked" );
-    } 
-    else if( button10.checked ) 
-    { 
-      console.log( "10 checked" );
-    } 
-    else if( buttonSurprise.checked ) 
-    { 
-      console.log( "surprise checked" );
-    } 
-    else
-    { 
-      console.log( "nothing" );
-    } 
+    let apiKey = "05BEAHZ43U53";
+    console.log( gifs.data.results[0].url );
+    gifForm.addEventListener ( "submit", event=> 
+    {
+      event.preventDefault();
+      const searchTitle = document.createElement( "p" );
+      searchTitle.textContent = searchGifs.value;
+      gifResults.appendChild( searchTitle );
+  
+      console.log( searchGifs.value );
+      if( button1.checked ) 
+      { 
+        const image = document.createElement( "img" );
+        image.src = gifs.data.results[0].url + "&key=" + apiKey;
+        gifResults.appendChild( image );    
+      }
+      else if( button4.checked ) 
+      { 
+        console.log( "4 checked" );
+      } 
+      else if( button10.checked ) 
+      { 
+        console.log( "10 checked" );
+      } 
+      else if( buttonSurprise.checked ) 
+      { 
+        console.log( "surprise checked" );
+      } 
+      else
+      { 
+        console.log( "nothing" );
+      } 
+    } );
   } );
 } )();
